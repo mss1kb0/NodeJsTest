c99766b5f2b7b3006f701b93d8f220bcf9e95dbf
mss1kb0 <mss1kb0@gmail.com>
2021-12-24 17:03:35 +0900

    a

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+//const express = require('express')
+//const path = require('path')
+//const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+//express()
+//  .use(express.static(path.join(__dirname, 'public')))
+//  .set('views', path.join(__dirname, 'views'))
+//  .set('view engine', 'ejs')
+//  .get('/', (req, res) => res.render('pages/index'))
+//  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+const { spawn } = require('child_process');
+const got = require('got');
+const test = require('tape');
+
+// Start the app
+const env = Object.assign({}, process.env, { PORT: 5000 });
+const child = spawn('node', ['index.js'], { env });
+
+test('responds to requests', (t) => {
+    t.plan(4);
+
+    // Wait until the server is ready
+    child.stdout.on('data', _ => {
+        // Make a request to our app
+        (async () => {
+            const response = await got('http://127.0.0.1:5000');
+            // stop the server
+            child.kill();
+            // No error
+            t.false(response.error);
+            // Successful response
+            t.equal(response.statusCode, 200);
+            // Assert content checks
+            t.notEqual(response.body.indexOf("<title>Node.js Getting Started on Heroku</title>"), -1);
+            t.notEqual(response.body.indexOf("Getting Started on Heroku with Node.js"), -1);
+        })();
+    });
+});
