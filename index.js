f8cc2c87fe57b6ef7c040dba8eff96ea6e590310
mss1kb0 <mss1kb0@gmail.com>
2021-12-24 17:18:51 +0900

    a

--- index.js
+++ index.js
@@ -9,31 +9,18 @@
 //  .get('/', (req, res) => res.render('pages/index'))
 //  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-const { spawn } = require('child_process');
-const got = require('got');
-const test = require('tape');
+const http = require('http');
+const fs = require('fs');
+const ejs = require('ejs');
 
-// Start the app
-const env = Object.assign({}, process.env, { PORT: 5000 });
-const child = spawn('node', ['index.js'], { env });
+const index_page = fs.readFileSync('./pages/index.ejs', 'UTF8');
+var server = http.createServer(getFromClient);
 
-test('responds to requests', (t) => {
-    t.plan(4);
+server.listen(5000);
 
-    // Wait until the server is ready
-    child.stdout.on('data', _ => {
-        // Make a request to our app
-        (async () => {
-            const response = await got('http://127.0.0.1:5000');
-            // stop the server
-            child.kill();
-            // No error
-            t.false(response.error);
-            // Successful response
-            t.equal(response.statusCode, 200);
-            // Assert content checks
-            t.notEqual(response.body.indexOf("<title>Node.js Getting Started on Heroku</title>"), -1);
-            t.notEqual(response.body.indexOf("Getting Started on Heroku with Node.js"), -1);
-        })();
-    });
-});
+function getFromClient(request, response) {
+    var content = ejs.render(index_page, { content: "“à—e‚Í‚±‚ê‚É‚È‚é‚Í‚¸‚Å‚·" });
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.write(content);
+    response.end();
+}
