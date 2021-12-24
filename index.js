3215f643b3968044c4cbad40103728fdd787be23
mss1kb0 <mss1kb0@gmail.com>
2021-12-24 17:33:00 +0900

    s

--- index.js
+++ index.js
@@ -1,26 +1,10 @@
-//const express = require('express')
-//const path = require('path')
-//const PORT = process.env.PORT || 5000
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-//express()
-//  .use(express.static(path.join(__dirname, 'public')))
-//  .set('views', path.join(__dirname, 'views'))
-//  .set('view engine', 'ejs')
-//  .get('/', (req, res) => res.render('pages/index'))
-//  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
-const http = require('http');
-const fs = require('fs');
-const ejs = require('ejs');
-
-const index_page = fs.readFileSync('./index.ejs', 'UTF8');
-var server = http.createServer(getFromClient);
-
-server.listen(5000);
-
-function getFromClient(request, response) {
-    var content = ejs.render(index_page, { content: "“à—e‚Í‚±‚ê‚É‚È‚é‚Í‚¸‚Å‚·" });
-    response.writeHead(200, { 'Content-Type': 'text/html' });
-    response.write(content);
-    response.end();
-}
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('./index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
